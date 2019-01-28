ace4f096ffd51d9f9c98d1e59e36d8781b1ddaa1
cal <callummac@protonmail.com>
2019-01-28 09:00:06 +0000

    Added sass compiler and raw-loader to monitor html.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,5 @@
+require('../scss/main.scss');
+
+if (process.env.NODE_ENV === 'development') require('../../public/index.html');
+
 console.log('app.js running');
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
@@ -16,8 +17,29 @@ module.exports = {
     plugins: [
         new webpack.ProvidePlugin({
             Vue: 'vue'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/main.css',
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: "raw-loader"
+            }
+        ],
+    },
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
